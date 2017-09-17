bc0c91a496ef8d684b279f940df91615ea613023
cedricde <cedricde@users.noreply.github.com>
2017-09-17 09:13:10 +0200

    Add copyright line

--- src/openvpn/comp-rohc.c
+++ src/openvpn/comp-rohc.c
@@ -7,6 +7,7 @@
  *
  *  Copyright (C) 2002-2017 OpenVPN Technologies, Inc. <sales@openvpn.net>
  *  Copyright (C) 2010-2011 Didier Barvaux <didier@barvaux.org>
+ *  Copyright (C) 2017 Cédric Delmas <cedricde@outlook.fr>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License version 2
